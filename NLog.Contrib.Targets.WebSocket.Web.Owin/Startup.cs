3f4473b172702717072738eb9976709110ab56fe
Samuel89Su <sushoucai1989@hotmail.com>
2021-07-25 12:02:52 +0800

    owin middleware

---
+++ NLog.Contrib.Targets.WebSocket.Web.Owin/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using Owin.WebSocket.Extensions;
+using System;
+using System.IO;
+
+[assembly: OwinStartup(typeof(NLog.Contrib.Targets.WebSocket.Web.Owin.Startup))]
+
+namespace NLog.Contrib.Targets.WebSocket.Web.Owin
+{
+    public class Startup
+    {
+        private const string Path_Viewer = "websocketloggerviewer";
+
+        private static readonly string BaseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path_Viewer);
+
+        private const string BasePath = "/wslogger";
+        private const string Path_Listen = BasePath + "/listen";
+
+
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+
+            var fileServerOptions = new FileServerOptions
+            {
+                RequestPath = new PathString("/" + Path_Viewer),
+                FileSystem = new PhysicalFileSystem(BaseDir),
+            };
+            app.UseFileServer(fileServerOptions);
+
+            //For static routes http://foo.com/ws use MapWebSocketRoute and attribute the WebSocketConnection with [WebSocketRoute('/ws')]
+            app.MapWebSocketRoute<WebSocketLogListener>(Path_Listen);
+        }
+    }
+}
---
+++ NLog.Contrib.Targets.WebSocket.Web.Owin/WebSocketLogListener.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Owin;
+using NLog;
+using NLog.Contrib.Targets.WebSocket;
+using Owin.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NLog.Contrib.Targets.WebSocket.Web.Owin
+{
+    public class WebSocketLogListener : WebSocketConnection
+    {
+        public override async Task OnMessageReceived(ArraySegment<byte> message, WebSocketMessageType type)
+        {
+            switch (type)
+            {
+                case WebSocketMessageType.Text:
+                    break;
+                case WebSocketMessageType.Binary:
+                    break;
+                case WebSocketMessageType.Close:
+                    break;
+                default:
+                    break;
+            }
+
+            var cMessage = Encoding.UTF8.GetString(message.Array, message.Offset, message.Count);
+
+            foreach (var target in LogManager.Configuration.ConfiguredNamedTargets)
+            {
+                if (target is WebSocketServerTarget webSocketServerTarget)
+                {
+                    await webSocketServerTarget.AcceptWebSocketCommands(cMessage, mWebSocket);
+                }
+            }
+        }
+
+        public override void OnOpen()
+        {
+            foreach (var target in LogManager.Configuration.ConfiguredNamedTargets)
+            {
+                if (target is WebSocketServerTarget webSocketServerTarget)
+                {
+                    webSocketServerTarget.Subscribe(mWebSocket);
+                }
+            }
+        }
+        public override void OnClose(WebSocketCloseStatus? closeStatus, string closeStatusDescription)
+        {
+            foreach (var target in LogManager.Configuration.ConfiguredNamedTargets)
+            {
+                if (target is WebSocketServerTarget webSocketServerTarget)
+                {
+                    webSocketServerTarget.Subscribe(mWebSocket);
+                }
+            }
+        }
+        public override bool AuthenticateRequest(IOwinRequest request)
+        {
+            return true;
+        }
+    }
+}
